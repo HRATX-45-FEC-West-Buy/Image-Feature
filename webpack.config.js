98436fe49bc6ad839a21485739ae5856fa1b19ee
David Silva <davidsilva2841+accounts+gitthub@gmail.com>
2019-12-09 12:53:26 -0600

    Fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,63 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client');
-var DIST_DIR = path.join(__dirname, '/public');
+// var SRC_DIR = path.join(__dirname, '/client');
+// var DIST_DIR = path.join(__dirname, '/public');
+const path = require('path');
 
-module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+const DIST_FOLDER = path.resolve(__dirname, 'public');
+const ENTRY_FILE = './client/index.jsx';
 
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  }
+const config = {
+    entry: ENTRY_FILE,
+    output: {
+        filename: 'bundle.js',
+        path: DIST_FOLDER,
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
 };
+
+if (process.env.NODE_ENV === 'development') {
+    config['mode'] = 'development';
+    config['watch'] = true;
+    config['devServer'] = {
+        contentBase: DIST_FOLDER,
+        compress: true,
+        port: 9000
+    };
+} else if (process.env.NODE_ENV === 'production') {
+    config['mode'] = 'production';
+}
+
+module.exports = config;
+
+
